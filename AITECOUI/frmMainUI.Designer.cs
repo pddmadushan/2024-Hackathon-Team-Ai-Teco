443f76c41f1e661283f76a26f1b35c7251e6d87b
Kosala.Samarasinghe <Kosala.Samarasinghe@acumatica.com>
2024-10-25 23:34:15 +0530

    Initial commit

---
+++ AITECOUI/frmMainUI.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace AITECOUI
+{
+    partial class frmMainUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblGenerateBtn = new System.Windows.Forms.Label();
+            this.lblCreateFileBtn = new System.Windows.Forms.Label();
+            this.lblCloseBtn = new System.Windows.Forms.Label();
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.txtLLM = new System.Windows.Forms.TextBox();
+            this.txtOpenAI = new System.Windows.Forms.TextBox();
+            this.rbLLM = new System.Windows.Forms.RadioButton();
+            this.rbOpenAI = new System.Windows.Forms.RadioButton();
+            this.lblShowHideCtrl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.BackColor = System.Drawing.Color.White;
+            this.txtInput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.txtInput.Location = new System.Drawing.Point(27, 275);
+            this.txtInput.Margin = new System.Windows.Forms.Padding(2);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(320, 108);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.Text = "Describe the test scenario you want to create";
+            this.txtInput.Enter += new System.EventHandler(this.txtInput_Enter);
+            this.txtInput.Leave += new System.EventHandler(this.txtInput_Leave);
+            // 
+            // lblGenerateBtn
+            // 
+            this.lblGenerateBtn.BackColor = System.Drawing.Color.Transparent;
+            this.lblGenerateBtn.Image = global::AITECOUI.Properties.Resources.G1;
+            this.lblGenerateBtn.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.lblGenerateBtn.Location = new System.Drawing.Point(112, 398);
+            this.lblGenerateBtn.Name = "lblGenerateBtn";
+            this.lblGenerateBtn.Size = new System.Drawing.Size(126, 43);
+            this.lblGenerateBtn.TabIndex = 6;
+            this.lblGenerateBtn.Click += new System.EventHandler(this.lblGenerateBtn_Click);
+            this.lblGenerateBtn.MouseLeave += new System.EventHandler(this.lblGenerateBtn_MouseLeave);
+            this.lblGenerateBtn.MouseHover += new System.EventHandler(this.lblGenerateBtn_MouseHover);
+            // 
+            // lblCreateFileBtn
+            // 
+            this.lblCreateFileBtn.BackColor = System.Drawing.Color.Transparent;
+            this.lblCreateFileBtn.Image = global::AITECOUI.Properties.Resources.C11;
+            this.lblCreateFileBtn.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.lblCreateFileBtn.Location = new System.Drawing.Point(635, 572);
+            this.lblCreateFileBtn.Name = "lblCreateFileBtn";
+            this.lblCreateFileBtn.Size = new System.Drawing.Size(129, 45);
+            this.lblCreateFileBtn.TabIndex = 6;
+            this.lblCreateFileBtn.Click += new System.EventHandler(this.lblCreateFileBtn_Click);
+            this.lblCreateFileBtn.MouseLeave += new System.EventHandler(this.lblCreateFileBtn_MouseLeave);
+            this.lblCreateFileBtn.MouseHover += new System.EventHandler(this.lblCreateFileBtn_MouseHover);
+            // 
+            // lblCloseBtn
+            // 
+            this.lblCloseBtn.BackColor = System.Drawing.Color.Transparent;
+            this.lblCloseBtn.Image = global::AITECOUI.Properties.Resources.Close;
+            this.lblCloseBtn.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.lblCloseBtn.Location = new System.Drawing.Point(955, -3);
+            this.lblCloseBtn.Name = "lblCloseBtn";
+            this.lblCloseBtn.Size = new System.Drawing.Size(40, 39);
+            this.lblCloseBtn.TabIndex = 6;
+            this.lblCloseBtn.Click += new System.EventHandler(this.lblCloseBtn_Click);
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(249)))), ((int)(((byte)(249)))));
+            this.rtbOutput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtbOutput.Location = new System.Drawing.Point(477, 155);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.Size = new System.Drawing.Size(423, 399);
+            this.rtbOutput.TabIndex = 7;
+            this.rtbOutput.Text = "";
+            this.rtbOutput.TextChanged += new System.EventHandler(this.rtbOutput_TextChanged);
+            // 
+            // txtLLM
+            // 
+            this.txtLLM.Location = new System.Drawing.Point(123, 623);
+            this.txtLLM.Name = "txtLLM";
+            this.txtLLM.Size = new System.Drawing.Size(241, 20);
+            this.txtLLM.TabIndex = 8;
+            this.txtLLM.Visible = false;
+            // 
+            // txtOpenAI
+            // 
+            this.txtOpenAI.Location = new System.Drawing.Point(123, 600);
+            this.txtOpenAI.Name = "txtOpenAI";
+            this.txtOpenAI.Size = new System.Drawing.Size(241, 20);
+            this.txtOpenAI.TabIndex = 8;
+            this.txtOpenAI.Visible = false;
+            // 
+            // rbLLM
+            // 
+            this.rbLLM.AutoSize = true;
+            this.rbLLM.Location = new System.Drawing.Point(36, 625);
+            this.rbLLM.Name = "rbLLM";
+            this.rbLLM.Size = new System.Drawing.Size(71, 17);
+            this.rbLLM.TabIndex = 9;
+            this.rbLLM.Text = "LLM URL";
+            this.rbLLM.UseVisualStyleBackColor = true;
+            this.rbLLM.Visible = false;
+            // 
+            // rbOpenAI
+            // 
+            this.rbOpenAI.AutoSize = true;
+            this.rbOpenAI.Checked = true;
+            this.rbOpenAI.Location = new System.Drawing.Point(36, 603);
+            this.rbOpenAI.Name = "rbOpenAI";
+            this.rbOpenAI.Size = new System.Drawing.Size(86, 17);
+            this.rbOpenAI.TabIndex = 9;
+            this.rbOpenAI.TabStop = true;
+            this.rbOpenAI.Text = "OpenAI URL";
+            this.rbOpenAI.UseVisualStyleBackColor = true;
+            this.rbOpenAI.Visible = false;
+            // 
+            // lblShowHideCtrl
+            // 
+            this.lblShowHideCtrl.Location = new System.Drawing.Point(2, 625);
+            this.lblShowHideCtrl.Name = "lblShowHideCtrl";
+            this.lblShowHideCtrl.Size = new System.Drawing.Size(28, 17);
+            this.lblShowHideCtrl.TabIndex = 10;
+            this.lblShowHideCtrl.Click += new System.EventHandler(this.lblShowHideCtrl_Click);
+            // 
+            // frmMainUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::AITECOUI.Properties.Resources.MainUIBG2;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1000, 645);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblShowHideCtrl);
+            this.Controls.Add(this.rbOpenAI);
+            this.Controls.Add(this.rbLLM);
+            this.Controls.Add(this.txtOpenAI);
+            this.Controls.Add(this.txtLLM);
+            this.Controls.Add(this.rtbOutput);
+            this.Controls.Add(this.lblCloseBtn);
+            this.Controls.Add(this.lblCreateFileBtn);
+            this.Controls.Add(this.lblGenerateBtn);
+            this.Controls.Add(this.txtInput);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMainUI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.frmMainUI_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblGenerateBtn;
+        private System.Windows.Forms.Label lblCreateFileBtn;
+        private System.Windows.Forms.Label lblCloseBtn;
+        private System.Windows.Forms.RichTextBox rtbOutput;
+        private System.Windows.Forms.TextBox txtLLM;
+        private System.Windows.Forms.TextBox txtOpenAI;
+        private System.Windows.Forms.RadioButton rbLLM;
+        private System.Windows.Forms.RadioButton rbOpenAI;
+        private System.Windows.Forms.Label lblShowHideCtrl;
+    }
+}
+
---
+++ AITECOUI/frmMainUI.cs
@@ -0,0 +1,263 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Security.Policy;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace AITECOUI
+{
+    public partial class frmMainUI : Form
+    {
+        public frmMainUI()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+            Application.Exit();
+        }
+
+        private void frmMainUI_Load(object sender, EventArgs e)
+        {
+            txtLLM.Text = "https://249e-35-236-212-91.ngrok-free.app/generate_response";
+            txtOpenAI.Text = "https://249e-35-236-212-91.ngrok-free.app/generate_response";
+        }
+
+        private async void btnGenerateCodeFile_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnSavetoFile_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtInput_Enter(object sender, EventArgs e)
+        {
+            if (txtInput.Text== "Describe the test scenario you want to create")
+            {
+                txtInput.Clear();
+                txtInput.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtInput_Leave(object sender, EventArgs e)
+        {
+            if (txtInput.Text == "")
+            {
+                txtInput.Text= "Describe the test scenario you want to create";
+                txtInput.ForeColor = Color.Gray;
+            }
+        }
+        public class AIResultModel
+        {
+            public string response { get; set; }
+        }
+
+        public class AIRequestModel
+        {
+            public string userQuery { get; set; }
+        }
+
+        private void processAIResult(string aiResult, char lineBreak)
+        {
+            string[] aiResponseArray = aiResult.Split(lineBreak);
+            foreach (var line in aiResponseArray)
+            {
+                rtbOutput.Text += line + '\n';
+            }
+        }
+
+        private void processAIResult(string aiResult)
+        {
+            rtbOutput.Text = aiResult;
+        }
+
+        private async void lblGenerateBtn_Click(object sender, EventArgs e)
+        {
+            string content = GenerateContentFromAI();
+
+            if (rbOpenAI.Checked)
+            {
+                processAIResult(content);
+            }
+            if (rbLLM.Checked)
+            {
+                processAIResult(content, '$');
+            }
+        }
+
+        private string GenerateContentFromAI()
+        {
+            string url = "";
+            if (rbOpenAI.Checked)
+            {
+                url=txtOpenAI.Text;
+            }
+            if (rbLLM.Checked)
+            {
+                url = txtLLM.Text;
+            }
+
+            var payload = new AIRequestModel()
+            {
+                userQuery = txtInput.Text
+            };
+
+            // Serialize the object to JSON
+            string jsonPayload = JsonConvert.SerializeObject(payload);
+
+            using (HttpClient client = new HttpClient() { Timeout = TimeSpan.FromMinutes(10) })
+            {
+                try
+                {
+                    client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                    StringContent content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+
+                    HttpResponseMessage response = client.PostAsync(url, content).Result;
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = response.Content.ReadAsStringAsync().Result;
+                    AIResultModel aiResult = JsonConvert.DeserializeObject<AIResultModel>(responseBody);
+                    return aiResult.response.Replace("```csharp","").Replace("```","");
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+            }
+        }
+
+        private void lblGenerateBtn_MouseHover(object sender, EventArgs e)
+        {
+            lblGenerateBtn.Image = AITECOUI.Properties.Resources.G2;
+        }
+
+        private void lblGenerateBtn_MouseLeave(object sender, EventArgs e)
+        {
+            lblGenerateBtn.Image = AITECOUI.Properties.Resources.G1;
+        }
+
+        private void lblCreateFileBtn_MouseHover(object sender, EventArgs e)
+        {
+            lblCreateFileBtn.Image = AITECOUI.Properties.Resources.C22;
+        }
+
+        private void lblCreateFileBtn_MouseLeave(object sender, EventArgs e)
+        {
+            lblCreateFileBtn.Image = AITECOUI.Properties.Resources.C11;
+        }
+
+        private void lblCreateFileBtn_Click(object sender, EventArgs e)
+        {
+            Stream myStream;
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.Filter = "C# code files (*.cs)|*.cs|All files (*.*)|*.*";
+            saveFileDialog.FilterIndex = 1;
+            saveFileDialog.RestoreDirectory = true;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = saveFileDialog.OpenFile()) != null)
+                {
+                    myStream.Close();
+                    File.WriteAllText(saveFileDialog.FileName, rtbOutput.Text);
+                }
+            }
+        }
+
+        private void lblCloseBtn_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void rtbOutput_TextChanged(object sender, EventArgs e)
+        {
+            // getting keywords/functions
+            string keywords = @"\b(public|private|partial|static|namespace|class|using|void|foreach|in)\b";
+            MatchCollection keywordMatches = Regex.Matches(rtbOutput.Text, keywords);
+
+            // getting types/classes from the text 
+            string types = @"\b(Console)\b";
+            MatchCollection typeMatches = Regex.Matches(rtbOutput.Text, types);
+
+            // getting comments (inline or multiline)
+            string comments = @"(\/\/.+?$|\/\*.+?\*\/)";
+            MatchCollection commentMatches = Regex.Matches(rtbOutput.Text, comments, RegexOptions.Multiline);
+
+            // getting strings
+            string strings = "\".+?\"";
+            MatchCollection stringMatches = Regex.Matches(rtbOutput.Text, strings);
+
+            // saving the original caret position + forecolor
+            int originalIndex = rtbOutput.SelectionStart;
+            int originalLength = rtbOutput.SelectionLength;
+            Color originalColor = Color.Black;
+
+            // removes any previous highlighting (so modified words won't remain highlighted)
+            rtbOutput.SelectionStart = 0;
+            rtbOutput.SelectionLength = rtbOutput.Text.Length;
+            rtbOutput.SelectionColor = originalColor;
+
+            // scanning...
+            foreach (Match m in keywordMatches)
+            {
+                rtbOutput.SelectionStart = m.Index;
+                rtbOutput.SelectionLength = m.Length;
+                rtbOutput.SelectionColor = Color.Blue;
+            }
+
+            foreach (Match m in typeMatches)
+            {
+                rtbOutput.SelectionStart = m.Index;
+                rtbOutput.SelectionLength = m.Length;
+                rtbOutput.SelectionColor = Color.DarkCyan;
+            }
+
+            foreach (Match m in commentMatches)
+            {
+                rtbOutput.SelectionStart = m.Index;
+                rtbOutput.SelectionLength = m.Length;
+                rtbOutput.SelectionColor = Color.Green;
+            }
+
+            foreach (Match m in stringMatches)
+            {
+                rtbOutput.SelectionStart = m.Index;
+                rtbOutput.SelectionLength = m.Length;
+                rtbOutput.SelectionColor = Color.Brown;
+            }
+
+            // restoring the original colors, for further writing
+            rtbOutput.SelectionStart = originalIndex;
+            rtbOutput.SelectionLength = originalLength;
+            rtbOutput.SelectionColor = originalColor;
+
+            // giving back the focus
+            rtbOutput.Focus();
+        }
+
+        private void lblShowHideCtrl_Click(object sender, EventArgs e)
+        {
+            if (rbLLM.Visible == true) { rbLLM.Visible = false; } else { rbLLM.Visible = true; }
+            if (txtLLM.Visible == true) { txtLLM.Visible = false; } else { txtLLM.Visible = true; }
+            if (rbOpenAI.Visible == true) { rbOpenAI.Visible = false; }else { rbOpenAI.Visible = true; }
+            if (txtOpenAI.Visible == true) { txtOpenAI.Visible = false; } else { txtOpenAI.Visible = true; }
+        }
+    }
+}
